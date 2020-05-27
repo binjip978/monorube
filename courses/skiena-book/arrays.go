74d303f310d2d9ebe72f4a3dc7d6db7450d41c07
binjip978 <binjip978@gmail.com>
2020-05-27 12:29:29 +0300

    skiena

--- courses/skiena-book/arrays.go
+++ courses/skiena-book/arrays.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func pivot(array []int, index int) {
 	array[0], array[index] = array[index], array[0]
 
@@ -30,4 +32,75 @@ func dutchFlag(array []int) {
 			j++
 		}
 	}
+}
+
+func advance(array []int) bool {
+	return false
+}
+
+func deleteDubsFromSortedArray1(array []int) []int {
+	if len(array) <= 1 {
+		return array
+	}
+
+	var newArray []int
+	newArray = append(newArray, array[0])
+	prev := newArray[0]
+
+	for i := 1; i < len(array); i++ {
+		if array[i] != prev {
+			newArray = append(newArray, array[i])
+			prev = array[i]
+		}
+	}
+
+	return newArray
+}
+
+// deleteDubsFromSortedArray2 returns number of valid elements
+func deleteDubsFromSortedArray2(array []int) int {
+	writeIndex := 1
+	for i := 0; i < len(array); i++ {
+		if array[i] != array[writeIndex-1] {
+			array[writeIndex] = array[i]
+			writeIndex++
+		}
+	}
+
+	return writeIndex
+}
+
+func sellStockOnce(price []int) int {
+	min := price[0]
+	diff := 0
+
+	for i := 1; i < len(price); i++ {
+		cd := price[i] - min
+		if cd > diff {
+			diff = cd
+		}
+	}
+
+	return diff
+}
+
+func primes(n int) []int {
+	var p = func(v int) bool {
+		for i := 2; i <= int(math.Sqrt(float64(v))); i++ {
+			if v%i == 0 {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	var res []int
+	for i := 1; i <= n; i++ {
+		if p(i) {
+			res = append(res, i)
+		}
+	}
+
+	return res
 }
--- courses/skiena-book/main.go
+++ courses/skiena-book/main.go
@@ -3,7 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	ar := []int{1, 0, 0, 1, 2, 2, 1, 1, 0}
-	dutchFlag(ar)
-	fmt.Println(ar)
+	fmt.Println(primes(20))
 }
