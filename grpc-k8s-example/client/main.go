0ee742ee3f8eb0873c3eb48ac60bc904527b39f8
binjip978 <binjip978@gmail.com>
2020-08-30 20:01:39 +0300

    stuff

--- grpc-k8s-example/client/main.go
+++ grpc-k8s-example/client/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"google.golang.org/grpc"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/binjip978/monorube/grpc-k8s-example/server/service"
 )
 
 type cfg struct {
@@ -15,7 +22,31 @@ func parseCfg() cfg {
 	return cfg{serverAddr: *serverAddr}
 }
 
+func getID() string {
+	hostname, err := os.Hostname()
+	if err != nil {
+		return "unknown-hostname"
+	}
+	return hostname
+}
+
 func main() {
 	cfg := parseCfg()
 	fmt.Printf("send data to the server in a loop via gRPC, with current config %+v\n", cfg)
+	conn, err := grpc.Dial(cfg.serverAddr, grpc.WithInsecure())
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	client := service.NewMonitorClient(conn)
+	id := getID()
+	for {
+		client.Point(context.Background(), &service.PointRequest{
+			Id: id,
+			X:  rand.Int31(),
+			Y:  rand.Int31(),
+			Z:  rand.Int31(),
+		})
+		time.Sleep(10 * time.Second)
+	}
 }
