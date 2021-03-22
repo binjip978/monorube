9af648cfb33ac601fa6fa9a16a07f3fafa91b566
binjip978 <binjip978@gmail.com>
2021-03-22 22:15:18 +0300

    ingress stuff

--- other/hello-world/main.go
+++ other/hello-world/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/hello/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World Old"))
+	})
+
+	http.HandleFunc("/v1/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("sleeping for a 2 seconds")
+		time.Sleep(2 * time.Second)
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello World"))
+		w.Write([]byte("v1 Hello World"))
+	})
+
+	http.HandleFunc("/v2/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("sleeping for a 2 minutes")
+		time.Sleep(2 * time.Minute)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("v2 Hello World Sleepy"))
+	})
+
+	http.HandleFunc("/v3/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("fast version, no sleep at all")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("v3 Hello World Fast"))
 	})
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
